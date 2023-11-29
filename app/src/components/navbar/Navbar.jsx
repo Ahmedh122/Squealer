65e96699a5f13fa020b67f01b70bf04a1f21725b
G4njalf <federico.turrini@studio.unibo.it>
2023-11-29 18:26:05 +0100

    bog

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -10,41 +10,11 @@ import { AuthContext } from '../../context/Authcontext';
 import { useQuery } from 'react-query';
 import { makeRequest } from '../../axios';
 import { useLocation } from 'react-router';
-import Dropdown from 'react-bootstrap/Dropdown';
-
-
-  /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
 
 
 const Navbar = () => {
 
-
-
-
-  const userId = useLocation().pathname.split("/")[2];
-  const { data } = useQuery(["users"], () =>
-    makeRequest.get("/users/find/" + userId).then((res) => {
-      return res.data;
-    })
-  );
+  const {currentUser} = useContext(AuthContext);
   return (
     <div className='Navbar'>
       <div className='leftside'>
@@ -64,9 +34,9 @@ const Navbar = () => {
       <div className='rightside'>
         <SettingsIcon />
         <div className="user">
-          <img src={data.profilePic} alt="" />
-          <Link to={`/profile/${data._id}`} style={{ textDecoration: 'none', color: '#000' }}>
-            <span>{data.username}</span>
+          <img src={currentUser.profilePic} alt="" />
+          <Link to={`/profile/${currentUser._id}`} style={{ textDecoration: 'none', color: '#000' }}>
+            <span>{currentUser.username}</span>
           </Link>
         </div>
       </div>
