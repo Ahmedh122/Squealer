115e9588bb07829fc49fd56ca73cddef1d6cba4f
G4njalf <federico.turrini@studio.unibo.it>
2023-12-02 15:30:46 +0100

    Co-authored-by: KyoZD <DhahriAhmed@users.noreply.github.com>

--- app/src/components/share/Share.jsx
+++ app/src/components/share/Share.jsx
@@ -2,32 +2,80 @@ import "./share.css";
 import Image from "../../assets/img.png";
 import Map from "../../assets/Mappe.png";
 import Friend from "../../assets/Amici.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/Authcontext";
+import { useMutation, useQueryClient } from "react-query";
+import { makeRequest } from "../../axios";
 
 
 const Share = () => {
+  const [file, setFile] = useState(null);
+  const [desc, setDesc] = useState("");
 
-  const {currentUser} = useContext(AuthContext)
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await makeRequest.post("/upload", formData);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const { currentUser } = useContext(AuthContext);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (newPost) => {
+      return makeRequest.post("/posts", newPost);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["posts"]);
+      },
+    }
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    let imgUrl = "";
+    if (file) imgUrl = await upload();
+    mutation.mutate({ desc, img: imgUrl });
+    setDesc("");
+    setFile(null);
+  };
+
   return (
     <div className="Share">
       <div className="containerShare">
-        <div className="topShare">
-        <button>Public Squeal</button>
-        <button>Channel Squeal</button>
-        </div>
-        <hr />
         <div className="middleShare">
-          <img
-            src={currentUser.profilePic}
-            alt=""
-          />
-          <input type="text" placeholder={`What's on your mind ?`} />
+          <div className="leftShare">
+            <img src={"/upload/" + currentUser.profilePic} alt="" />
+            <input
+              type="text"
+              placeholder={`What's on your mind ${currentUser.name}?`}
+              onChange={(e) => setDesc(e.target.value)}
+              value={desc}
+            />
+          </div>
+          <div className="rightShare">
+            {file && (
+              <img className="fileShare" alt="" src={URL.createObjectURL(file)} />
+            )}
+          </div>
         </div>
         <hr />
         <div className="bottomShare">
           <div className="leftShare">
-            <input type="file" id="file" style={{display:"none"}} />
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
             <label htmlFor="file">
               <div className="itemShare">
                 <img src={Image} alt="" />
@@ -44,7 +92,7 @@ const Share = () => {
             </div>
           </div>
           <div className="rightShare">
-            <button>Share</button>
+            <button onClick={handleClick}>Share</button>
           </div>
         </div>
       </div>
