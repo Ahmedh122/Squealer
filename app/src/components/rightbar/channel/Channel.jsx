3cc621001e5ebd1ecca2fa541f3664cafa5f5747
G4njalf <federico.turrini@studio.unibo.it>
2023-12-20 18:29:42 +0100

    Co-authored-by: KyoZD <DhahriAhmed@users.noreply.github.com>

--- app/src/components/rightbar/channel/Channel.jsx
+++ app/src/components/rightbar/channel/Channel.jsx
@@ -1,24 +1,25 @@
 import "./channel.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink,Route } from "react-router-dom";
 
 const Channel = ({ channel }) => {
+
   return (
     <div className="userRB">
-      {channel && (
-        <>
-          <div className="userInfoRB">
-            <img src="" alt="" />
-            <p>
-              <Link
-                to={`/channel/${channel.channelname}`}
+       {channel && (
+            <>
+      <div className="userInfoRB">
+        <img src="" alt="" />
+        <p>
+            <Link
+                to={`/channel/${channel.channelname}`} 
                 style={{ textDecoration: "none", color: "inherit" }}
-              >
-                <span>{channel.channelname}</span>
-              </Link>
-            </p>
-          </div>
-        </>
-      )}
+            > 
+          <span>{channel.channelname}</span>
+          </Link>
+        </p>
+      </div>
+      </>
+       )}
     </div>
   );
 };
