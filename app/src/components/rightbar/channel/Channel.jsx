54495defd6d2da10c878b19aaddc7f2eb41343c2
G4njalf <federico.turrini@studio.unibo.it>
2023-12-22 16:49:09 +0100

    Co-authored-by: KyoZD <DhahriAhmed@users.noreply.github.com>

--- app/src/components/rightbar/channel/Channel.jsx
+++ app/src/components/rightbar/channel/Channel.jsx
@@ -1,25 +1,28 @@
 import "./channel.css";
-import { Link, NavLink,Route } from "react-router-dom";
+import React from "react";
 
 const Channel = ({ channel }) => {
+  const redirectToChannel = (channelname) => {
+    window.location.href = `/channel/${channelname}`;
+  };
 
   return (
     <div className="userRB">
-       {channel && (
-            <>
-      <div className="userInfoRB">
-        <img src="" alt="" />
-        <p>
-            <Link
-                to={`/channel/${channel.channelname}`} 
-                style={{ textDecoration: "none", color: "inherit" }}
-            > 
-          <span>{channel.channelname}</span>
-          </Link>
-        </p>
-      </div>
-      </>
-       )}
+      {channel && (
+        <>
+          <div className="userInfoRB">
+            <img src="" alt="" />
+            <p>
+              <span
+                onClick={() => redirectToChannel(channel.channelname)}
+                style={{ cursor: "pointer", textDecoration: "none", color: "inherit" }}
+              >
+                {channel.channelname}
+              </span>
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
